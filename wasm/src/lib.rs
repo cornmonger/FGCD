1ca90f1b58f87151c2e0eea96e2d96b63b9f854f
roylaurie <roy.laurie.pub@asmov.software>
2023-09-27 15:48:26 -0700

    fetch working

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -6,6 +6,9 @@ use chrono;
 use serde_wasm_bindgen;
 use fgcd_model as model;
 use fgcd_parse;
+use web_sys;
+use wasm_bindgen_futures;
+use js_sys;
 
 #[wasm_bindgen]
 extern "C" {
@@ -13,20 +16,25 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn greet() -> Result<JsValue, JsValue> {
+pub async fn greet() -> Result<JsValue, JsValue> {
     set_panic_hook();
 
-    /*let game_profile = model::game::Profile::new(
-        String::from("Game Name"),
-        String::from("Publishing House"),
-        String::from("Big Corp"),
-        chrono::Utc::now().date_naive(),
-        String::from("http://foo.com"),
-        String::from("http://wikipedia.org"),
-        vec![String::from("Xbox"), String::from("Gameboy")]
-    );*/
+    let window = web_sys::window().unwrap();
+    let request = web_sys::Request::new_with_str("Game.bin").unwrap();
+    let response = wasm_bindgen_futures::JsFuture::from(window.fetch_with_request(&request)).await?;
+    assert!(response.is_instance_of::<web_sys::Response>());
+    let response: web_sys::Response = response.dyn_into().unwrap();
 
-    let game = fgcd_parse::read_game_bin("Game.bin");
-    //let game = model::game::Game::new(game_profile);
+    let buf_promise = response.array_buffer().unwrap();
+    let buf = wasm_bindgen_futures::JsFuture::from(buf_promise)
+        .await
+        .unwrap();
+    assert!(buf.is_instance_of::<js_sys::ArrayBuffer>());
+
+    let typebuf: js_sys::Uint8Array = js_sys::Uint8Array::new(&buf);
+    let mut bytes = vec![0; typebuf.length() as usize];
+    typebuf.copy_to(&mut bytes[..]);
+
+    let game = fgcd_parse::read_game_bytes(&bytes);
     Ok(serde_wasm_bindgen::to_value(&game)?)
 }
